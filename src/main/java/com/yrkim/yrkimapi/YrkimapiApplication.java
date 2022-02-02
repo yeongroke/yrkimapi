c437ab98ac96be90c3cd37e498a20f998a9941af
yeongroke <chfaos120@naver.com>
2022-02-02 14:43:55 +0900

    init

---
+++ src/main/java/com/yrkim/yrkimapi/YrkimapiApplication.java
@@ -0,0 +1,13 @@
+package com.yrkim.yrkimapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YrkimapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YrkimapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yrkim/yrkimapi/YrkimapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrkim.yrkimapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YrkimapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

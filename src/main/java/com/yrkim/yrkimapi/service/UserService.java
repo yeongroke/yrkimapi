3040411c875d3949311bd60d5b7ba139f863236b
yeongroke <chfaos120@naver.com>
2022-02-09 21:23:03 +0900

    feat: user service 로직 추가

--- src/main/java/com/yrkim/yrkimapi/service/UserService.java
+++ src/main/java/com/yrkim/yrkimapi/service/UserService.java
@@ -1,5 +1,16 @@
 package com.yrkim.yrkimapi.service;
 
-public interface UserService {
+import com.yrkim.yrkimapi.model.dto.UserDto;
+import com.yrkim.yrkimapi.model.entity.User;
+import com.yrkim.yrkimapi.model.response.CommonResult;
+import com.yrkim.yrkimapi.model.response.ListResult;
+import com.yrkim.yrkimapi.model.response.SingleResult;
+import org.springframework.data.domain.Pageable;
 
+
+public interface UserService {
+    SingleResult<User> save(UserDto user);
+    SingleResult<User> getUser(long id);
+    ListResult<User> getUsers(Pageable pageable);
+    CommonResult delete(long id);
 }
--- src/main/java/com/yrkim/yrkimapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/yrkim/yrkimapi/service/impl/UserServiceImpl.java
@@ -1,16 +1,47 @@
 package com.yrkim.yrkimapi.service.impl;
 
+import com.yrkim.yrkimapi.exception.UserNotFoundException;
+import com.yrkim.yrkimapi.model.dto.UserDto;
+import com.yrkim.yrkimapi.model.entity.User;
+import com.yrkim.yrkimapi.model.response.CommonResult;
+import com.yrkim.yrkimapi.model.response.ListResult;
+import com.yrkim.yrkimapi.model.response.SingleResult;
 import com.yrkim.yrkimapi.repository.jpa.UserRepository;
+import com.yrkim.yrkimapi.security.JwtTokenUtil;
+import com.yrkim.yrkimapi.service.CommonResponseService;
 import com.yrkim.yrkimapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
 @Slf4j
-@Service
+@Service("userService")
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final CommonResponseService commonResponseService;
 
+    @Override
+    public SingleResult<User> save(UserDto user) {
+        return commonResponseService.getSingleResult(userRepository.save(user.toEntity()));
+    }
 
+    @Override
+    public SingleResult<User> getUser(long id) {
+        return commonResponseService.getSingleResult(userRepository.findById(id).orElseThrow(UserNotFoundException::new));
+    }
+
+    @Override
+    public ListResult<User> getUsers(Pageable pageable) {
+        return commonResponseService .getListResult(userRepository.findAll(pageable));
+    }
+
+    @Override
+    public CommonResult delete(long id) {
+        userRepository.deleteById(id);
+        return commonResponseService.getSuccessResult();
+    }
 }
